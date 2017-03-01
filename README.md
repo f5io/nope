285c41750a6d7d447796492e02849c9052ccf90d
Joe Harlow <joe@f5.io>
2017-03-01 03:20:31 +0000

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Installation
+
+```
+curl -o- https://raw.githubusercontent.com/f5io/nope/master/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+version="1.0.0"
 dir=$(cd $(dirname $0) && pwd)
-url="https://github.com/f5io/nope/releases/download/1.0.0/nope.tar.gz"
+url="https://github.com/f5io/nope/releases/download/$version/nope.tar.gz"
 
+echo "👌  fetching binary for nope $version"
 curl -sL $url | tar -zxf - -C /usr/local/bin
+echo "🎉  successfully installed nope $version"
+
+
 
