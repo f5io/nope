c0884b4a10680c375f9f73c5234fda8e8b3a1d2e
Joe Harlow <joe@f5.io>
2017-03-01 03:12:07 +0000

    add release url

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -e
 
 dir=$(cd $(dirname $0) && pwd)
-url="https://raw/nope.tar.gz"
+url="https://github.com/f5io/nope/releases/download/1.0.0/nope.tar.gz"
 
-curl $url | tar -zxf -C /usr/local/bin
+curl -sL $url | tar -zxf - -C /usr/local/bin
 
